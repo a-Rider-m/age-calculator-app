320132678e574d0e99ad86961f50c5c410db9d3d
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2023-08-31 17:55:41 -0500

    styles and first js lines

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+var fechaActual = new Date(Date());
+var añoActual = fechaActual.getFullYear();
+var mesActual = fechaActual.getMonth();
+var diaActual = fechaActual.getDate();
+
+console.log(fechaActual);
+console.log(añoActual);
+console.log(mesActual);
+console.log(diaActual);
