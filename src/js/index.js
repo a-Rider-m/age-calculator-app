7e077b4775940648c1afc3071426a015c25a853f
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2023-09-01 19:26:02 -0500

    calculateAge function 01 09 23

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 const inputs = document.querySelectorAll(".dateimputs");
 const form = document.querySelector("#form");
 const arrowButton = document.querySelector("#arrow-button");
-const currentDate = new Date(Date());
 
 function validateForm(e) {
     switch(e.target.id) {
@@ -52,9 +51,70 @@ function validateField(idName, labelId, textId, input, maxNumber, maxLength) {
     }
 }
 
-var fechaActual = new Date(Date());
-var añoActual = fechaActual.getFullYear();
-var mesActual = fechaActual.getMonth();
-var diaActual = fechaActual.getDate();
+function calculateAge() {
+    const currentDate = new Date(Date());
+    const currentYear = currentDate.getFullYear();
 
-console.log(currentDate)
+    //Le sumo 1, porque hoy 1 de septiembre me lo registra como mes 8
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentDay = currentDate.getDate();
+
+    console.log(currentMonth);
+    const birthYear = 2005;
+    const birthMonth = 4;
+    const birthDay = 30;
+
+    let years = calculateYears();
+    let months = calculateMonths();
+    let days = calculateDays();
+
+    //Calculate years
+    function calculateYears() {
+        let years = currentYear - birthYear;
+        let months = currentMonth - birthMonth;
+        
+        if(months < 0 || (months === 0 && currentMonth < birthMonth)) {
+            years--;
+        }
+
+        return years;
+    }
+
+    function calculateMonths() {
+        let months = 0;
+        
+        if(currentMonth > birthMonth) {
+            months = currentMonth - birthMonth;
+        } else if(currentMonth < birthMonth) {
+            months = 12 - (birthMonth - currentMonth);
+        } else if (currentMonth == birthMonth && currentDay > birthDay) {
+            if(currentMonth - birthMonth == 0) {
+                months == 0;
+            } else {
+                months == 11;
+            }
+        }
+        
+        return months;
+
+    }
+
+    function calculateDays() {
+        let day = currentDay - birthDay;
+        if(day < 0) {
+            months = (months - 1 < 0) ? 11 : months - 1;
+
+            //supongo que el valor debe ser 30 o 31 dependiendo del mes
+            day = 31 + day;
+        }
+
+        return day;
+        
+    }
+
+    console.log(years);
+    console.log(months);
+    console.log(days);
+}
+
+calculateAge();
